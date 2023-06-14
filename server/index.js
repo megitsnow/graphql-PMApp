738e8db0b9338b73a5fa2e06b97cf5311bc9b8f7
Megan Snow <megansnow@Megans-MacBook-Air.local>
2023-06-14 12:31:31 -0700

    started database

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
+const colors = require('colors');
+const connectDB = require('./config/db')
 
 const app = express();
+
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema, 
