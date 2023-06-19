d87729397d8d7c2df49b8a16d05ab34fc0a18862
Megan Snow <megansnow@Megans-MacBook-Air.local>
2023-06-19 15:12:13 -0700

    started on frontend of project

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
 const port = process.env.PORT || 5000;
@@ -9,6 +10,8 @@ const connectDB = require('./config/db')
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema, 
